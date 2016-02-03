504b5ca09ea2be74d0b4010249068c337f5dff58
ibrahimlawal <ibrahim@lawal.me>
2016-02-03 13:10:02 +0100

    Starting up

--- src/SkeletonClass.php
+++ src/SkeletonClass.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace League\Skeleton;
+namespace League\Paystack;
 
 class SkeletonClass
 {
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace League\Skeleton\Test;
+namespace League\Paystack\Test;
 
 class ExampleTest extends \PHPUnit_Framework_TestCase
 {
