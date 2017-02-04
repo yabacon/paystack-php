7342f754d5c4b05cab9ea3a99c389325965b411f
ibrahimlawal <ibrahim@lawal.me>
2017-02-04 10:32:29 +0100

    [tests] 100% coverage on RequestBuilder

--- tests/Http/RequestBuilderTest.php
+++ tests/Http/RequestBuilderTest.php
@@ -4,6 +4,8 @@ namespace Yabacon\Paystack\Tests\Http;
 use Yabacon\Paystack\Http\RequestBuilder;
 use Yabacon\Paystack;
 use Yabacon\Paystack\Contracts\RouteInterface;
+use Yabacon\Paystack\Routes\Customer;
+use Yabacon\Paystack\Routes\Transaction;
 
 class RequestBuilderTest extends \PHPUnit_Framework_TestCase
 {
@@ -28,5 +30,36 @@ class RequestBuilderTest extends \PHPUnit_Framework_TestCase
 
         $rb->putArgsIntoEndpoint($endpoint);
         $this->assertEquals('verify/some', $endpoint);
+    }
+
+    public function testBuild()
+    {
+        $p = new Paystack('sk_');
+        $params = ['email'=>'some@ema.il'];
+        $rb = new RequestBuilder($p, Customer::create(), $params);
+
+        $r = $rb->build();
+        $this->assertEquals('https://api.paystack.co/customer', $r->endpoint);
+        $this->assertEquals('Bearer sk_', $r->headers['Authorization']);
+        $this->assertEquals('post', $r->method);
+        $this->assertEquals(json_encode($params), $r->body);
+
+        $params = ['perPage'=>10];
+        $rb = new RequestBuilder($p, Customer::getList(), $params);
+
+        $r = $rb->build();
+        $this->assertEquals('https://api.paystack.co/customer?perPage=10', $r->endpoint);
+        $this->assertEquals('Bearer sk_', $r->headers['Authorization']);
+        $this->assertEquals('get', $r->method);
+        $this->assertEmpty($r->body);
+
+        $args = ['reference'=>'some-reference'];
+        $rb = new RequestBuilder($p, Transaction::verify(), [], $args);
+
+        $r = $rb->build();
+        $this->assertEquals('https://api.paystack.co/transaction/verify/some-reference', $r->endpoint);
+        $this->assertEquals('Bearer sk_', $r->headers['Authorization']);
+        $this->assertEquals('get', $r->method);
+        $this->assertEmpty($r->body);
     }
 }
