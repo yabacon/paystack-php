820020894fc7633f93030861a0863dd11723dbe1
ibrahimlawal <ibrahim@lawal.me>
2017-03-08 16:21:32 +0100

    ci-fix

--- src/autoload.php
+++ src/autoload.php
@@ -6,7 +6,7 @@
  */
 spl_autoload_register(
     function ($class_name) {
-        if(strpos($class_name, 'Yabacon\Paystack')===0){
+        if (strpos($class_name, 'Yabacon\Paystack')===0) {
             $file = dirname(__FILE__) . DIRECTORY_SEPARATOR;
             $file .= str_replace([ 'Yabacon\\', '\\' ], ['', DIRECTORY_SEPARATOR ], $class_name) . '.php';
             include_once $file;
