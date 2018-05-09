a49a9fa56cb1c80f8739d832ca56f498f93bdab6
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-05-09 08:14:19 -0500

    Update Invoice.php

--- src/Paystack/Routes/Invoice.php
+++ src/Paystack/Routes/Invoice.php
@@ -1,3 +1,27 @@
 <?php
 
-    namespace ;
+    namespace Yabacon\Paystack\Routes;
+
+    use Yabacon\Paystack\Contracts\RouteInterface;
+
+    class Invoice implements RouteInterface
+    {
+            public static function root()
+            {
+                
+                return '/paymentrequest';
+            }
+        
+            public static function create(){
+            
+            }
+        
+            public static function getList()
+            {
+            
+                    return [ RouteInterface::METHOD_KEY   => RouteInterface::GET_METHOD,
+                    RouteInterface::ENDPOINT_KEY => Invoice::root() ];
+            }
+    }
+
+?>
