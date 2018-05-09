2e7ed866b80e1c067295d5a75ad7e0311422cf65
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-05-09 08:48:52 -0500

    Update Invoice.php

--- src/Paystack/Routes/Invoice.php
+++ src/Paystack/Routes/Invoice.php
@@ -45,15 +45,57 @@
             {
             
                     return [ RouteInterface::METHOD_KEY   => RouteInterface::GET_METHOD,
-                    RouteInterface::ENDPOINT_KEY => Invoice::root(),
+                    RouteInterface::ENDPOINT_KEY => Invoice::root() ];
+            }
+        
+            public static function verify(){
+            
+                    return [ RouteInterface::METHOD_KEY   => RouteInterface::GET_METHOD,
+                    RouteInterface::ENDPOINT_KEY => Invoice::root() . '/verify/{invoice_id_or_code}',
+                    RouteInterface::ARGS_KEY     => ['invoice_id_or_code' ]
+                   ];
+            }
+        
+            public static function notify(){
+            
+                    return [RouteInterface::METHOD_KEY   => RouteInterface::POST_METHOD,
+                    RouteInterface::ENDPOINT_KEY => Invoice::root() . 'notify/{invoice_id_or_code}' ];
+            }
+        
+            public static function metrics(){
+            
+                    return [ RouteInterface::METHOD_KEY   => RouteInterface::GET_METHOD,
+                    RouteInterface::ENDPOINT_KEY => Invoice::root() . '/totals' ];
+            }
+        
+            public static function finalize(){
+            
+                    return [RouteInterface::METHOD_KEY   => RouteInterface::POST_METHOD,
+                    RouteInterface::ENDPOINT_KEY => Invoice::root() . 'finalize/{invoice_id_or_code}' ];
+            }
+        
+            public static function update(){
+            
+                    return [RouteInterface::METHOD_KEY   => RouteInterface::PUT_METHOD,
+                    RouteInterface::ENDPOINT_KEY => Invoice::root() . 'update/{invoice_id_or_code}',
                     RouteInterface::PARAMS_KEY   => [
+                            'line_items',
+                            'description',
+                            'amount',
                             'customer',
-                            'status',
-                            'currency',
-                            'paid',
-                            'include_archive'
-                        ]
+                            'send_notification',
+                            'tax',
+                            'due_date',
+                            'metadata',
+                            'currency'
+                        ]       
                     ];
+            }
+        
+            public static function archive(){
+            
+                    return [RouteInterface::METHOD_KEY   => RouteInterface::POST_METHOD,
+                    RouteInterface::ENDPOINT_KEY => Invoice::root() . 'archive/{invoice_id_or_code}' ];
             }
     }
 
